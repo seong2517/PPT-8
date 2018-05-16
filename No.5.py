d72ea29833cf15d0463313c9f584970422321c6e
seong2517 <35947690+seong2517@users.noreply.github.com>
2018-05-16 14:19:20 +0900

    Add files via upload

---
+++ No.5.py
@@ -0,0 +1,19 @@
+items = {"라면":650, "우유":1100, "콜라":1200, "캔커피":500, "과자":700}
+
+s = 0
+
+it = input ("제품명 :")
+
+while True :
+    if it in items :
+        s = s+items[it]
+        print("[%s:%d] > %d"%(it,items[it],s))
+        it = input("제품명 : ")
+    elif it !="" and it not in items :
+        print(it,"는 미등록 제품입니다.")
+        it = input("제품명 : ")
+        continue
+    elif it == "":
+        break
+
+print("총 금액 : ",s)
